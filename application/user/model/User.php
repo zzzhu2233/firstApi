4fc95eda4b6ad8580d6b4f4ae2955e9461814356
zzzhu2233 <1393316046@qq.com>
2020-07-14 10:25:31 +0800

    token

--- application/user/model/User.php
+++ application/user/model/User.php
@@ -5,6 +5,7 @@ namespace app\user\model;
 
 
 use baseAll\model\BaseMod;
+use baseAll\model\MyRedis;
 use think\Db;
 
 class User extends BaseMod
@@ -17,6 +18,32 @@ class User extends BaseMod
     public function getOneInfoWhere($where){
         $userInfo = Db::table('user')->where($where)->find();
         return $userInfo;
+    }
+
+    /**
+     * @desc 获取redis中key
+     * @update zrr
+     * @time 2020/7/14
+     * @param $key string 需要获取的key名称
+    */
+    public function getToken($key){
+        $myRedis = new MyRedis();
+        $keyValue = $myRedis->get('test1');
+        dump($keyValue);
+    }
+
+    /**
+     * @desc 设置redis中key
+     * @update zrr
+     * @time 2020/7/14
+     * @param $key string 需要设置的key名称
+     * @param $value string 需要设置的key值
+     * @param $activeTime integer 单位秒，key的有效时间
+     */
+    public function setToken($key,$value,$activeTime){
+        $myRedis = new MyRedis();
+        $keyValue = $myRedis->set('test1','简体中文是否可以','7200');
+        dump($keyValue);
     }
 
 }
---
+++ baseAll/model/MyRedis.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace baseAll\model;
+
+
+use think\cache\driver\Redis;
+
+class MyRedis extends Redis
+{
+
+}
